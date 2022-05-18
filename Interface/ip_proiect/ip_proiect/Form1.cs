1240d627f504456881534ca6c6c327f651fa5f71
iulianacuptor <iuliana-stefania.cuptor@student.tuiasi.ro>
2022-05-18 16:25:34 +0300

    interfata

---
+++ Interface/ip_proiect/ip_proiect/Form1.cs
@@ -0,0 +1,51 @@
+﻿using DataBaseManager;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PharmacyManagementDLL;
+using Items;
+using Exceptions;
+using System.Data.SqlClient;
+using Items.DataBaseObjects;
+using Items.Commons;
+
+namespace ip_proiect
+{
+    public partial class Form1 : Form
+    {
+        private DB _dbInstance;
+        private Permissions _permissions;
+        private ProxyActionManager _util;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void login_Click(object sender, EventArgs e)
+        {
+            string username = user.Text;
+            string password = pass.Text;
+            //_dbInstance = DB.GetInstance("IPpharma.db");
+            _util = new ProxyActionManager();
+            /*if (_util.Login(username, password)==true){
+                Form pagina2 = new Form2();
+                this.Hide();
+                pagina2.Show();
+            }*/
+            if (username == "1" && password == "1")
+            {
+                Form pagina2 = new Form2();
+                this.Hide();
+                pagina2.Show();
+            }
+        }
+    }
+}
---
+++ Interface/ip_proiect/ip_proiect/Form2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ip_proiect
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void logOut_Click(object sender, EventArgs e)
+        {
+            Form pagina = new Form1();
+            this.Hide();
+            pagina.Show();
+        }
+
+        private void name_TextChanged(object sender, EventArgs e)
+        { }
+        
+    }
+}
