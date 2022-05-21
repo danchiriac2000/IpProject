eb4565b7a9d0580a4aff480659914b7020455044
Gabriela Ursachi <gabriela.ursachi@student.tuiasi.ro>
2022-05-21 14:20:32 +0300

    Framework version changed

--- PharmacyManagementDLL/IActionManager.cs
+++ PharmacyManagementDLL/IActionManager.cs
@@ -33,27 +33,27 @@ namespace PharmacyManagementDLL
         /// Adds a new product to the existing ones.
         /// </summary> 
         /// <param name="newProduct">Product to be added</param>     
-        public void AddNewProduct(Product newProduct);
+        void AddNewProduct(Product newProduct);
 
         /// <summary>
         /// Sell products to clients.
         /// </summary>
         /// <param name="barcode">Unique identifier of product.</param>
         /// <param name="quantity">Product amount which will be sold. </param>
-        public void SellProduct(int barcode, int quantity);
+        void SellProduct(int barcode, int quantity);
 
         /// <summary>
         /// Update product stock in database .
         /// </summary>
         /// <param name="barcode">Unique identifier of product</param>
         /// <param name="quantity">Value to be added to the current stock. </param>
-        public void AddToStock(int barcode,int quantity);           
+        void AddToStock(int barcode,int quantity);           
 
         /// <summary>
         /// Queries for obtain a list of all existing products.
         /// </summary>
         /// <returns>All existing products.</returns>
-        public List<Product> GetProducts();
+        List<Product> GetProducts();
 
         /// <summary>
         /// Add new employee profile to database.
@@ -61,7 +61,7 @@ namespace PharmacyManagementDLL
         /// <param name="username">Newly added username</param>
         /// /// <param name="password">Password for this profile</param>
         /// /// <param name="occupationCode">A specific code given to the user's function in pharmacy</param>  
-        public void AddUser(string username,string password,int occupationCode);
+        void AddUser(string username,string password,int occupationCode);
 
         /// <summary>
         /// Modifies user password in database.
@@ -69,26 +69,26 @@ namespace PharmacyManagementDLL
         /// <param name="userID">Modified userID.</param>
         /// <param name="oldPass">Old password for this account.</param>
         /// <param name="newPass">New password for this account.</param>
-        public void UpdateUserPassword(int userID,string oldPass,string newPass);
+        void UpdateUserPassword(int userID,string oldPass,string newPass);
 
         /// <summary>
         /// Gets info about a user with the given id
         /// </summary>
         /// <param name="userID">ID of the user we want info about</param>
         /// <returns>Requested user or null if it doesn't exist.</returns>
-        public User GetUser(int userID);
+        User GetUser(int userID);
 
         /// <summary>
         /// Removes a user only if it exists.
         /// </summary>
         /// <param name="userID">ID of the user we want to remove</param>
-        public void DeleteUser(int userID);
+        void DeleteUser(int userID);
 
         /// <summary>
         /// Queries database to display all existing users.
         /// </summary>
         /// <returns>All existing users</returns>
-        public List<User> GetUsers();
+        List<User> GetUsers();
 
 
 
